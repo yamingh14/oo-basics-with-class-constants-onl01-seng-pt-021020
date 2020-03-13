0ccd113a87162430aef4b43d6d94e123e17aa8d0
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-13 18:48:06 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,16 @@
 class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
+  @@all = []
 
-  def initialize(title)
+  def initialize(title, genre)
+    @genre = genre
     @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def turn_page
