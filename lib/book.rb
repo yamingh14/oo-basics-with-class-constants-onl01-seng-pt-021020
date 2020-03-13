df0726b3a1a6b477348f5505e9a3b3d2edf75a48
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-13 18:49:48 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,20 +1,22 @@
-class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
-  @@all = []
 
-  def initialize(title, genre)
-    @genre = genre
-    @title = title
-    @@all << self
-  end
 
-  def self.all
-    @@all
-  end
-
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
-
-end
+# class Book
+#   attr_accessor :author, :page_count, :genre
+#   attr_reader :title
+#   @@all = []
+# 
+#   def initialize(title, genre)
+#     @genre = genre
+#     @title = title
+#     @@all << self
+#   end
+# 
+#   def self.all
+#     @@all
+#   end
+# 
+#   def turn_page
+#     puts "Flipping the page...wow, you read fast!"
+#   end
+# 
+# end
