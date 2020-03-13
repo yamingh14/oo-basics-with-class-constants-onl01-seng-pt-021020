505a05cd7379422ec9a4a0af30f9484351394389
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-13 18:49:50 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -4,19 +4,19 @@
 #   attr_accessor :author, :page_count, :genre
 #   attr_reader :title
 #   @@all = []
-# 
+#
 #   def initialize(title, genre)
 #     @genre = genre
 #     @title = title
 #     @@all << self
 #   end
-# 
+#
 #   def self.all
 #     @@all
 #   end
-# 
+#
 #   def turn_page
 #     puts "Flipping the page...wow, you read fast!"
 #   end
-# 
+#
 # end
